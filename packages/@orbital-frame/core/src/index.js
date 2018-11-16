7237083f299add596716fce143b4baf77de99b8f
Bremen Braun <bremen.braun@gmail.com>
2018-11-15 20:56:08 -0600

    more iterations of endless config

--- packages/@orbital-frame/core/src/index.js
+++
@@ -1,7 +0,0 @@
-import lifecycle from './lifecycle'
-import serviceHost from './service/host'
-
-export default robot => {
-  serviceHost.instantiate(robot) // initialize services
-  lifecycle.run() // lifecycle phases communicate via services
-}
---
+++ packages/orbital-frame-core/src/index.js
@@ -0,0 +1,8 @@
+// import lifecycle from './lifecycle'
+// import serviceHost from './service/host'
+
+export default robot => {
+  console.log('yeeee')
+  // serviceHost.instantiate(robot) // initialize services
+  // lifecycle.run() // lifecycle phases communicate via services
+}
