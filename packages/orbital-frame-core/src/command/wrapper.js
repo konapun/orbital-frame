5ba464a1c0c3a40dc2d9c5dbe81e9a8a0df95247
Bremen Braun <bremen.braun@gmail.com>
2019-05-05 22:33:36 -0500

    fix wrapper execution calling context

--- packages/orbital-frame-core/src/command/wrapper.js
+++ packages/orbital-frame-core/src/command/wrapper.js
@@ -1,6 +1,6 @@
 export default definition => ({
   ...definition,
   execute (args, opts = {}) {
-    return definition.execute([].concat(args), opts)
+    return definition.execute.call(this, [].concat(args), opts)
   }
 })
