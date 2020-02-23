9994888ef8ee6a3042994dfc7761dff48b8ba2bb
konapun <bremen.braun@gmail.com>
2020-02-22 21:44:39 -0600

    allow managing backgrounded interactions

--- packages/orbital-frame-core/src/service/__tests__/interaction.test.js
+++ packages/orbital-frame-core/src/service/__tests__/interaction.test.js
@@ -1,6 +1,6 @@
 import interactionService from '../interaction'
 
-let configService, jobService, listenerService, messengerService, interaction, listenCallback
+let configService, jobService, listenerService, listenerApi, messengerService, interaction, listenCallback
 
 beforeEach(() => {
   configService = {
@@ -14,19 +14,30 @@ beforeEach(() => {
     }))
   }
 
-  listenerService = {
-    listen () {
+  listenerApi = {
+    pipe: jest.fn(fn => {
+      listenCallback = fn
       return {
-        pipe (fn) {
-          listenCallback = fn
+        pipe: jest.fn(innerFn => {
+          innerFn({user: {id: 1}})
+          console.log('CALLED INNER PIPE!!!')
           return {
-            pipe: jest.fn(),
+            pipe: jest.fn(() => {
+              console.log('CALLED INNER INNER PIPE!')
+              return {
+                end: jest.fn()
+              }
+            }),
             end: jest.fn()
           }
-        },
+        }),
         end: jest.fn()
       }
-    }
+    }),
+    end: jest.fn()
+  }
+  listenerService = {
+    listen: jest.fn(() => listenerApi)
   }
 
   messengerService = {
@@ -51,11 +62,22 @@ describe('interaction service', () => {
     expect(messengerService.respond).toHaveBeenCalledWith(expect.any(Function), 'What is your age?')
   })
 
-  it('should require a ">" for interaction responses', () => {
-    // TODO:
+  it('should require a ">" for interaction responses', async () => {
+    await interaction.createInteractionChannel(1)
+    expect(listenerService.listen).toHaveBeenCalledWith('^>')
   })
 
-  it('shouldn\'t invoke the interaction channel on ">" messages for which there are no unfulfilled prompts', () => {
+  fit('shouldn\'t invoke the interaction channel on ">" messages for which there are no unfulfilled prompts', async () => {
+    const listener = listenerApi.pipe({ user: { id: 4 }, message: { text: 'konapun' } })
+    listener.pipe = jest.fn(() => {
+      console.log('CALLED PIPE!')
+    })
+    const interactionChannel = await interaction .createInteractionChannel(1)
+    const message = await interactionChannel.prompt('What is your name?')
+
+    console.log('Before pipe')
+    listenerApi.pipe()
+    console.log('After pipe')
     // TODO:
   })
 
@@ -73,5 +95,9 @@ describe('interaction service', () => {
     it('should listen to all users passed into prompt', async () => {
       // TODO:
     })
+  })
+
+  it('should allow changing the foregrounded job per-user', async () => {
+
   })
 })
